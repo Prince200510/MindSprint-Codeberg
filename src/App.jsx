6a6f9a686a4cb9d07d9c1cefd82c6c3c1e60b308
Prince200510 <princemaurya8879@gmail.com>
2025-09-05 15:41:17 +0530

    tailwind v4

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ function App() {
 
   return (
     <>
-      <div className="w-full py-8 bg-gradient-to-r from-sky-500 to-indigo-600 text-white text-center">
-        <h2 className="text-2xl font-semibold">hello world</h2>
-      </div>
+
     </>
   )
 }
